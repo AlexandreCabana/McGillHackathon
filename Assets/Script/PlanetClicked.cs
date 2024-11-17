1c3a106bf187f693f6540006537953ed902611a7
QuentinProulx <quentin.proulx@gmail.com>
2024-11-16 23:04:42 -0500

    You can now click on planets to view their properties (their mass)

---
+++ Assets/Script/PlanetClicked.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.Examples;
+using UnityEngine;
+
+public class PlanetClicked : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public PlanetProprety properties;
+    void Start(){
+        properties=gameObject.GetComponent<PlanetProprety>();
+        text = GameObject.Find("Canvas").GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    void OnMouseDown(){
+
+        text.text="Planet Mass: "+properties.weight.ToString();
+
+    }
+}
